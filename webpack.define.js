125d7283033b417c23f83fb2ab415ce2bcd7d0fe
Jacky <jacky8595125@gmail.com>
2019-01-21 12:59:21 +0800

    ...

--- webpack.define.js
+++ webpack.define.js
@@ -94,6 +94,42 @@ module.exports = {
         // 跟著HtmlWebpackHarddiskPlugin套件
         // alwaysWriteToDisk: true
       }),
+      new HtmlWebpackPlugin({
+        inject: false,
+        template: path.resolve(__dirname, 'ClientApp/ejs/index_2/fontLogin.ejs'),
+        filename: path.resolve(__dirname, 'fontLogin.html'),
+        chunks: ['index'],
+        HtmlWebpackPluginOverride: true,
+        // hash:true,//防止缓存
+        outputFile: {
+          vendor: 'wwwroot/vendor/dll.vendor.js',
+          isProd: isProd,
+          port: module.exports.devServerPort,
+        },
+        minify: minify,
+        // 啟用手動排序
+        chunksSortMode: 'manual'
+        // 跟著HtmlWebpackHarddiskPlugin套件
+        // alwaysWriteToDisk: true
+      }),
+      new HtmlWebpackPlugin({
+        inject: false,
+        template: path.resolve(__dirname, 'ClientApp/ejs/index_2/memberCE.ejs'),
+        filename: path.resolve(__dirname, 'memberCE.html'),
+        chunks: ['index'],
+        HtmlWebpackPluginOverride: true,
+        // hash:true,//防止缓存
+        outputFile: {
+          vendor: 'wwwroot/vendor/dll.vendor.js',
+          isProd: isProd,
+          port: module.exports.devServerPort,
+        },
+        minify: minify,
+        // 啟用手動排序
+        chunksSortMode: 'manual'
+        // 跟著HtmlWebpackHarddiskPlugin套件
+        // alwaysWriteToDisk: true
+      }),
     )
 
     if (enableHtmlWebpackHarddiskPlugin) {
