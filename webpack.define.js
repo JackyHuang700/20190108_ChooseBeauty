547deb36d85f191e75bebbb6e47dfe859351b1a9
Jacky <jacky8595125@gmail.com>
2019-01-23 16:08:41 +0800

    ...

--- webpack.define.js
+++ webpack.define.js
@@ -150,6 +150,26 @@ module.exports = {
         // 跟著HtmlWebpackHarddiskPlugin套件
         // alwaysWriteToDisk: true
       }),
+      
+      // 禮物紀錄
+      new HtmlWebpackPlugin({
+        inject: false,
+        template: path.resolve(__dirname, 'ClientApp/ejs/index_2/giftRecord.ejs'),
+        filename: path.resolve(__dirname, 'giftRecord.html'),
+        chunks: ['index'],
+        HtmlWebpackPluginOverride: true,
+        // hash:true,//防止缓存
+        outputFile: {
+          vendor: 'wwwroot/vendor/dll.vendor.js',
+          isProd: isProd,
+          port: module.exports.devServerPort,
+        },
+        minify: minify,
+        // 啟用手動排序
+        chunksSortMode: 'manual'
+        // 跟著HtmlWebpackHarddiskPlugin套件
+        // alwaysWriteToDisk: true
+      }),
     )
 
     if (enableHtmlWebpackHarddiskPlugin) {
